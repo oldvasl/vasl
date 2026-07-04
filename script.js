69f6580ff544059dd188ff9dc181e8000624adb5
Aghey <plusmastoreir@gmail.com>
2026-07-04 22:32:32 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let currentUser = null;
-let API = "https://vasling.alipviv8698.workers.dev";
+
 // ورود
 function login(){
 const u = document.getElementById("username").value;
@@ -44,51 +44,3 @@ feed.prepend(post);
 
 document.getElementById("postText").value = "";
 }
-export default {
-async fetch(request, env) {
-
-const url = new URL(request.url);
-
-// =========================
-// POST /post  (ساخت پست)
-// =========================
-if (url.pathname === "/post" && request.method === "POST") {
-
-const data = await request.json();
-
-const text = data.text || "";
-const user = data.user || "unknown";
-
-const message =
-`📌 POST
-👤 ${user}
-📝 ${text}
-`;
-
-await fetch(`https://api.telegram.org/bot${env.BOT_TOKEN}/sendMessage`, {
-method: "POST",
-headers: {"Content-Type": "application/json"},
-body: JSON.stringify({
-chat_id: env.CHAT_ID,
-text: message
-})
-});
-
-return new Response(JSON.stringify({
-ok: true,
-status: "posted"
-}), {
-headers: {"Content-Type": "application/json"}
-});
-}
-
-// =========================
-// GET /health (تست)
-// =========================
-if (url.pathname === "/health") {
-return new Response("Vasl API is running");
-}
-
-return new Response("Not Found", {status: 404});
-}
-};
